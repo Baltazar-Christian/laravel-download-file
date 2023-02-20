3e1153391efac84245d338e2bf22ea75078a3ba9
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2023-02-20 04:06:45 +0300

    minor change

--- app/Http/Controllers/DownloadFileController.php
+++ app/Http/Controllers/DownloadFileController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Response;
 
 class DownloadFileController extends Controller
 {
 
     // For all images
 
-    public function index(Type $var = null)
+    public function index()
     {
         $files = Storage::files("public");
         $imgFiles = array();
@@ -23,8 +24,10 @@ class DownloadFileController extends Controller
     //For Download function
     function downloadFile($file_name){
         $file = Storage::disk('public')->get($file_name);
-  
-        return (new Response($file, 200))
-              ->header('Content-Type', 'image/jpeg');
+        $path = 'public/' . $file_name;
+        // return (new Response($file, 200))
+        //       ->header('Content-Type', 'image/jpeg');
+
+        return Storage::download( $path );
     }
 }
